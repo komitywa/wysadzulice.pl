235776c4d164e5c23c23f47d4cd3fe6c702150d0
Tomasz Magulski <tomasz.magulski@gmail.com>
2015-09-12 18:48:23 +0200

    #11 - solution for CSRF problem + sending proper url for redirect

--- src/wysadzulice/wysadzulice_app/views.py
+++ src/wysadzulice/wysadzulice_app/views.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 from . import models
 
 
@@ -34,9 +35,9 @@ def new_planting(request, id_):
     return render(request, 'new_planting.html', context={'id_': id_})
 
 
+@csrf_exempt
 def create_planting(request, id_):
-    return '{}'
     c = models.Campaign.objects.get(id=id_)
     p = models.Planting(campaign=c)
     p.save()
-    return redirect('show_campaign', id_=id_)
+    return HttpResponse(u'{"url": "%s"}' % reverse('show_campaign', kwargs=dict(id_=id_)))
