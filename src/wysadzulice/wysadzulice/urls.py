ddfa9c1349b85a7305f2a79fa26a0119cabf6207
Tomasz Magulski <tomasz.magulski@gmail.com>
2015-10-14 22:51:30 +0200

    #33 fixing problem with urls patterns

--- src/wysadzulice/wysadzulice/urls.py
+++ src/wysadzulice/wysadzulice/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^', include('wysadzulice_app.urls')),
+    url(r'', include('wysadzulice_app.urls')),
 ]
