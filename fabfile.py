6ecbc06b5bf5303f7ad8e9af3fb98f225b674f70
Tomasz Magulski <tomasz.magulski@gmail.com>
2015-10-14 22:34:14 +0200

    #33 - fabfile for continous deployment + additional cleanup

---
+++ fabfile.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+u"""
+.. module:: fabfile
+Be aware, that becaus fabric doesn't support py3k You need to execute this
+particular script using Python 2.
+"""
+
+import contextlib
+
+from fabric.api import cd
+from fabric.api import env
+from fabric.api import prefix
+from fabric.api import run
+
+env.user = 'root'
+env.hosts = ['wysadzulice.pl']
+env.forward_agent = True
+
+
+def update():
+    u"""Function defining all steps required to properly update application."""
+
+    with contextlib.nested(
+        cd('/var/www/wysadzulice_pl'),
+        prefix('workon wysadzulice_pl')
+    ):
+        run('git pull')
+        run('git checkout master')
+
+    with contextlib.nested(
+        cd('/var/www/wysadzulice_pl/src/wysadzulice'),
+        prefix('workon wysadzulice_pl')
+    ):
+        run('python manage.py migrate --traceback')
+
+    run('service apache2 restart')
---
+++ src/wysadzulice/wysadzulice/wsgi_wysadzulicepl.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for wysadzulice project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+activate_this = '/root/.virtualenvs/wysadzulice_pl/bin/activate_this.py'
+exec(open(activate_this).read(), dict(__file__=activate_this))
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wysadzulice.settings")
+
+application = get_wsgi_application()
