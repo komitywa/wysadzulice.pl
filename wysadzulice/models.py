c3ffd7848fa4b0c967bab26de3ee53e1a60beb8c
Tomasz Magulski <tomasz.magulski@gmail.com>
2016-02-05 00:11:22 +0100

    #45 - models updated to current vision of what is campaign and planting

--- wysadzulice/models.py
+++ wysadzulice/models.py
@@ -15,12 +15,6 @@ class Campaign(models.Model):
 
 class Planting(models.Model):
     campaign = models.ForeignKey(Campaign)
-    lat = models.FloatField()
-    lng = models.FloatField()
-    zoom = models.FloatField()
-    heading = models.FloatField()
-    pitch = models.FloatField()
-    manifesto = models.CharField(max_length=200)
 
 
 class PlantedObject(models.Model):
@@ -28,6 +22,3 @@ class PlantedObject(models.Model):
     object_id = models.IntegerField()
     x = models.FloatField()
     y = models.FloatField()
-    scale = models.FloatField()
-    layer = models.IntegerField()
-    projection = models.IntegerField()
