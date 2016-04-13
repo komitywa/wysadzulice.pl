04268929962074abdddcd4f41a96faa110954ccb
rudymichal <majk.chudecki@gmail.com>
2016-04-13 20:11:48 +0200

    normalize URLs (#100)

--- wysadzulice/urls.py
+++ wysadzulice/urls.py
@@ -7,24 +7,24 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.mailing, name='mailing'),
-    url(r'^campaigns$', views.index, name='index'),
-    url(r'^campaigns/new$', views.new_campaign, name='new_campaign'),
+    url(r'^campaigns/$', views.index, name='index'),
+    url(r'^campaigns/new/$', views.new_campaign, name='new_campaign'),
     url(
-        r'^campaigns/(?P<id_>[0-9]+)$',
+        r'^campaigns/(?P<id_>[0-9]+)/$',
         views.show_campaign,
         name='show_campaign',
     ),
     url(
-        r'^campaigns/(?P<id_>[0-9]+)/plantings/new$',
+        r'^campaigns/(?P<id_>[0-9]+)/plantings/new/$',
         views.new_planting,
         name='new_planting',
     ),
     url(
         r'^campaigns/(?P<campaign_id>[0-9]+)'
-        r'/plantings/(?P<planting_id>[0-9]+)$',
+        r'/plantings/(?P<planting_id>[0-9]+)/$',
         views.show_planting,
         name='show_planting',
     ),
-    url(r'api/catalog$', views.catalog, name='catalog'),
-    url(r'api/manifesto/(?P<id_>[0-9]+)$', views.manifesto, name='manifesto'),
+    url(r'api/catalog/$', views.catalog, name='catalog'),
+    url(r'api/manifesto/(?P<id_>[0-9]+)/$', views.manifesto, name='manifesto'),
 ]
